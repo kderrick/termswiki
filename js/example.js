1d5fa32b1b01e262ac1ac0b69d87f528983ec72b
Kyle Derrick and Michelle Brecunier <kderrick1988@gmail.com>
2016-01-14 14:14:14 -0800

    add js functions to insert page headers and h3s

---
+++ js/example.js
@@ -0,0 +1,11 @@
+var name = "Kyle"
+var age = 27
+name +  ' is ' + age + ' years old'
+"Kyle is 27 years old"
+
+5 * 5
+25
+
+6%4
+2
+ß
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-// $(".code").text($(".code").html())
+$(document).ready(function() {
+var terms = ["Markdown", "Block Elements", "Inline Elements", "CSS", "Bootstrap", "Divs and Spans", "Floats", "Box Model", "Responsive Design", "Javascript", "Forms", "Branching", "Looping", "Arrays", "jQuery"];
+
+var pages = ["markdown", "block-elements", "inline-elements", "css", "bootstrap", "divs-spans", "floats", "boxmodel", "responsive-design", "javascript", "forms", "branching", "looping", "arrays", "jquery"];
+
+
+for(i = 0; i < terms.length; i += 1) {
+  if(window.location.href.indexOf(pages[i]) > -1) {
+    $("h1#term").append(terms[i]);
+  }
+
+  };
+
+  $(".definition").prepend("<h3>Definition</h3>");
+
+  $(".code").text($(".code").html());
+
+
+});
